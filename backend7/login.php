92ac3435e7b492c81cd74d2405cde4d7ca9d7d07
ilyamityaev02 <90443612+ilyamityaev02@users.noreply.github.com>
2022-05-22 19:53:27 +0300

    Add files via upload

---
+++ backend7/login.php
@@ -0,0 +1,62 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+session_start();
+include 'csrf.php';
+$csrf = new csrf();
+
+// Генерация id и значения токена
+$token_id = $csrf->get_token_id();
+$token_value = $csrf->get_token($token_id);
+
+if (!empty($_SESSION['login'])) {
+  header('Location: ./');
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  if (!empty($_GET['error']))
+  print("<div>Такого пользователя не существует!</div>");
+  if (!empty($_GET['wrong']))
+  print("<div>Неверный пароль!</div>");
+
+?>
+  
+<form action="" method="post">
+<input type="hidden" name="<?= $token_id; ?>" value="<?= $token_value; ?>" />
+<input name="login" placeholder="Логин"/>
+  <input name="pass" placeholder="Пароль"/>
+  <input type="submit" value="Войти" />
+</form>
+
+<?php
+}
+else {
+  if($csrf->check_valid('post')) {
+  print($_SERVER['REQUEST_METHOD']);
+  $user = 'u47522';
+	$pass = '7677055';
+	$db = new PDO('mysql:host=localhost;dbname=u47522', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $login = $_POST['login'];
+	try {
+	  $stmt = $db->prepare("SELECT * FROM heroes2 WHERE login = ?");
+	  $stmt -> execute([$login]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$row) {
+      header('Location: ?error=1&row='.$_POST['login']);
+      exit();
+    }
+    if(md5($_POST['pass']) !== $row['password']){
+      header('Location: ?wrong=1');
+      exit();
+    }
+	}
+	catch(PDOException $e){
+	  print('Error : ' . $e->getMessage());
+	  exit();
+	}
+  $_SESSION['login'] = $_POST['login'];
+  $_SESSION['uid'] = $row['id'];
+
+  header('Location: ./');
+}
+}
